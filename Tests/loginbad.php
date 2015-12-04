0011d633611ad2d9c22bc5d8ff65a755e41dbbd8
raresmr <rares@visual.ly>
2015-12-04 15:07:01 +0200

    add test that fails

---
+++ Tests/loginbad.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'vendor/phpunit/phpunit-selenium/PHPUnit/Extensions/SeleniumTestCase.php';
+require_once 'vendor/phpunit/phpunit-selenium/PHPUnit/Extensions/SeleniumTestSuite.php';
+require_once 'vendor/phpunit/phpunit-selenium/PHPUnit/Extensions/SeleniumTestCase/Driver.php';
+
+class loginbad extends PHPUnit_Extensions_SeleniumTestCase
+{
+  protected function setUp()
+  {
+    $this->setBrowser("*chrome");
+    $this->setBrowserUrl("http://127.0.0.1/");
+  }
+
+  public function testlogibad()
+  {
+    $this->open("/");
+    $this->type("id=edit-name", "adminx");
+    $this->type("id=edit-pass", "admin");
+    $this->selectWindow("null");
+    $this->click("id=edit-submit");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isElementPresent("link=Log out"));
+  }
+}
+?>
